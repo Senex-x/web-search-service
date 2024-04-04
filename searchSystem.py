690375f7c2d8ebd74420080b879b85993138d838
v.n.polyakov <v.n.polyakov@tinkoff.ru>
2024-04-04 14:17:41 +0300

    Finalize code

--- searchSystem.py
+++ searchSystem.py
@@ -17,47 +17,12 @@ class VectorSearch:
         self.lemmas = []
         self.matrix = None
         self.nlp = spacy.load("ru_core_news_sm")
-        self.read_links()
-        self.read_lemmas()
-        self.read_tf_idf()
-
-    def read_links(self):
-        with open(self.index_file_name, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-            for line in lines:
-                key, value = line.split(' ')
-                self.links[key] = value
-
-    def read_lemmas(self):
-        file_names = listdir(self.tf_idf_folder_name)
-        for file_name in file_names:
-            with open(self.tf_idf_folder_name + '/' + file_name, 'r', encoding='utf-8') as file:
-                lines = file.readlines()
-                for line in lines:
-                    self.lemmas.append(line.split(' ')[0])
-
-    def read_tf_idf(self):
-        file_names = listdir(self.tf_idf_folder_name)
-        self.matrix = np.zeros((len(file_names), len(self.lemmas)))
-        for file_name in file_names:
-            file_number = int(re.search('\\d+', file_name)[0])
-            with open(self.tf_idf_folder_name + '/' + file_name, 'r', encoding='utf-8') as file:
-                lines = file.readlines()
-                for i in range(len(lines)):
-                    lemma, idf, tf_idf = lines[i].split(' ')
-                    self.matrix[file_number - 1][i] = float(tf_idf)
-
-    def vectorize(self, query: str) -> np.ndarray:
-        vector = np.zeros(len(self.lemmas))
-        tokens = word_tokenize(query)
-        for token in tokens:
-            lemma = self.nlp(token)[0].lemma_
-            if lemma in self.lemmas:
-                vector[self.lemmas.index(lemma)] = 1
-        return vector
+        self.__load_article_links()
+        self.__load_lemmas()
+        self.__load_tf_idf()
 
     def search(self, query: str) -> list:
-        vector = self.vectorize(query)
+        vector = self.__vectorize(query)
         similarities = dict()
         i = 1
         for row in self.matrix:
@@ -68,6 +33,41 @@ class VectorSearch:
         sorted_similarities = sorted(similarities.items(), key=lambda item: item[1], reverse=True)
         result = [(self.links[str(doc[0])], doc[1]) for doc in sorted_similarities]
         return result
+
+    def __vectorize(self, query: str) -> np.ndarray:
+        vector = np.zeros(len(self.lemmas))
+        tokens = word_tokenize(query)
+        for token in tokens:
+            lemma = self.nlp(token)[0].lemma_
+            if lemma in self.lemmas:
+                vector[self.lemmas.index(lemma)] = 1
+        return vector
+
+    def __load_tf_idf(self):
+        file_names = listdir(self.tf_idf_folder_name)
+        self.matrix = np.zeros((len(file_names), len(self.lemmas)))
+        for file_name in file_names:
+            file_number = int(re.search('\\d+', file_name)[0])
+            with open(self.tf_idf_folder_name + '/' + file_name, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                for i in range(len(lines)):
+                    lemma, idf, tf_idf = lines[i].split(' ')
+                    self.matrix[file_number - 1][i] = float(tf_idf)
+
+    def __load_lemmas(self):
+        file_names = listdir(self.tf_idf_folder_name)
+        for file_name in file_names:
+            with open(self.tf_idf_folder_name + '/' + file_name, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                for line in lines:
+                    self.lemmas.append(line.split(' ')[0])
+
+    def __load_article_links(self):
+        with open(self.index_file_name, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            for line in lines:
+                key, value = line.split(' ')
+                self.links[key] = value
 
 
 if __name__ == '__main__':
